6ca74ccaa1130564d7173069a015d50af4782842
Tomaz Strukelj <tomaz.strukelj@dlabs.si>
2015-01-12 16:48:28 +0100

    fixed run position of shared/tmp

--- providers/rails.rb
+++ providers/rails.rb
@@ -148,12 +148,6 @@ action :before_symlink do
       environment new_resource.environment
     end
 
-    execute "(rm -Rf tmp ; ln -s ../../shared/tmp tmp)" do
-      cwd new_resource.release_path
-      user new_resource.owner
-      environment new_resource.environment
-    end
-
   if new_resource.precompile_assets.nil?
     new_resource.precompile_assets ::File.exists?(::File.join(new_resource.release_path, "config", "assets.yml"))
   end
@@ -169,6 +163,12 @@ action :before_symlink do
       environment new_resource.environment
     end
   end
+
+    execute "(rm -Rf tmp ; ln -s ../../shared/tmp tmp)" do
+      cwd new_resource.release_path
+      user new_resource.owner
+      environment new_resource.environment
+    end
 
 end
 
